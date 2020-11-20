ce2d6773794809430fdbcce4bb6c4dbd66672b52
Alvaro Orellana <tiveor@gmail.com>
2020-11-20 16:14:10 -0400

    VVG-CLI first version a CLI wrapper for VVG project

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
-    console.log("vvg-cli is in da house");
-    
+console.log("vvg-cli is cool");
+
+const vvg = require("@tiveor/vvg");
+const { createCanvas } = require("canvas");
+
+const getCommandByIndex = (index) => {
+  return process.argv.length > index ? process.argv[index] : "";
+};
+
+const random = () => {
+  const canvas = createCanvas(1024, 768);
+
+  const x = new vvg(canvas);
+
+  x.example4();
+  setTimeout(() => {
+    x.saveToFile("random.png");
+  }, 5000);
+};
+
+const runAction = () => {
+  const action = getCommandByIndex(2);
+
+  switch (action) {
+    case "random":
+      random();
+      break;
+    default:
+      console.log("please include an action to be executed");
+  }
+};
+
+runAction();
